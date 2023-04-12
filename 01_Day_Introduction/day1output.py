1b0d6a9001cab32077691d3e3be1c463675cb7be
ruumiia <78075902+ruumiia@users.noreply.github.com>
2023-04-12 19:38:38 +0800

    day 1 output

---
+++ 01_Day_Introduction/day1output.py
@@ -0,0 +1,34 @@
+
+#level 2
+
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 % 4)
+print(3 / 4)
+print(3 ** 4)
+print(3 // 4)
+
+print("Aaron")
+print("Vergara")
+print("Philippines")
+print("I am enjoying 30 days of python")
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type("Aaron"))
+print(type("Vergara"))
+print(type("Philippines"))
+
+#level 3
+
+print(type(True))
+print(type({'name':'mogus', 'age': '17'})) #dictionary
+print(type(('one', 'two', 'three'))) #tuple
+print(type({'pe','po','pu'})) #set
+
+import numpy as np
+print(np.sqrt((10-2)**2+(8-3)**2))
